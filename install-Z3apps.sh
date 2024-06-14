5a3fa18e8b4fec1983f271069c26916615251876
Z3R0C1PH3R <59080407+Z3R0C1PH3R@users.noreply.github.com>
2024-06-14 21:36:08 +0530

    Update install-Z3apps.sh

--- install-Z3apps.sh
+++ install-Z3apps.sh
@@ -6,12 +6,13 @@ exec >Z3apps-logfile.txt 2>&1
 apt install -y mpv wget git
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/yt-dlp
 chmod a+rx /usr/bin/yt-dlp  # Make executable
-ln -s /usr/bin/yt-dlp /usr/bin/youtube-dl
+ln -fs /usr/bin/yt-dlp /usr/bin/youtube-dl
 echo "ytdl-format=bestvideo[height<=?480][width<=?640]+bestaudio/best" >> /etc/mpv/mpv.conf 
 apt install -y python3.8
 rm /usr/bin/python3
-ln -s /usr/bin/python3.8 /usr/bin/python3
-mkdir temp
+ln -fs /usr/bin/python3.8 /usr/bin/python3
+mkdir -p temp
 git clone https://github.com/Z3R0C1PH3R/Z3apps.git temp
 cp -r temp/Z3apps temp/yt.sh temp/term.sh $progdir/
 rm -rf temp
+reboot
