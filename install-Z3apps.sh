e6a8ef00dd0ab4726a5aa2530a394e6108a330cf
Aryan Choudhary <59080407+Z3R0C1PH3R@users.noreply.github.com>
2024-10-05 03:14:24 +0530

    Update install-Z3apps.sh For new firmware

--- install-Z3apps.sh
+++ install-Z3apps.sh
@@ -3,9 +3,9 @@ set -e
 progdir=$(cd $(dirname $0); pwd)
 exec >Z3apps-logfile.txt 2>&1
 
-apt install -y mpv wget git imagemagick python3.8
+apt install -y mpv wget git imagemagick # python3.8
 
-ln -fs /usr/bin/python3.8 /usr/bin/python3
+# ln -fs /usr/bin/python3.8 /usr/bin/python3
 
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/yt-dlp
 chmod a+rx /usr/bin/yt-dlp  # Make executable
